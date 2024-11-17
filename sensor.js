b1c15175e98122c6cfc3d4dba858b12d4d3279de
aasim-s <aasimsayyed214@gmail.com>
2024-11-17 23:57:16 +0530

    dummy cars added

--- sensor.js
+++ sensor.js
@@ -9,15 +9,15 @@ class Sensor {
     this.readings = [];
   }
 
-  update(roadBorders) {
+  update(roadBorders, traffic) {
     this.#castRays();
     this.readings = [];
     for (let i = 0; i < this.rays.length; i++) {
-      this.readings.push(this.#getReading(this.rays[i], roadBorders));
+      this.readings.push(this.#getReading(this.rays[i], roadBorders, traffic));
     }
   }
 
-  #getReading(ray, roadBorders) {
+  #getReading(ray, roadBorders, traffic) {
     let intersections = [];
 
     for (let i = 0; i < roadBorders.length; i++) {
@@ -31,6 +31,22 @@ class Sensor {
         intersections.push(intersection);
       }
     }
+
+    for (let i = 0; i < traffic.length; i++) {
+      const poly = traffic[i].polygon;
+      for (let j = 0; j < poly.length; j++) {
+        const value = getIntersections(
+          ray[0],
+          ray[1],
+          poly[j],
+          poly[(j + 1) % poly.length]
+        );
+        if (value) {
+          intersections.push(value);
+        }
+      }
+    }
+
     if (intersections.length == 0) {
       return null;
     }
